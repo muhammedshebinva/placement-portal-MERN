4f4354c2e2fc7f5e33e49fd6112f0fcf341bf855
twinksaji223@gmail.com <muhammedshebinva@gmail.com>
2024-10-23 20:54:17 +0530

    updated

--- frontend/src/components/admin/ViewClass.jsx
+++ frontend/src/components/admin/ViewClass.jsx
@@ -19,7 +19,7 @@ function ViewClass() {
   return (
     <div>
     <Navbar/>
-    <h1>Training Class Schedule</h1>
+    <h1 class="text-4xl font-black text-gray-900 dark:text-white mb-5 mt-5">Training Class Schedule</h1>
     <ClassForm fetchClasses={fetchClasses} />
     <ClassList classes={classes} fetchClasses={fetchClasses} />
 
